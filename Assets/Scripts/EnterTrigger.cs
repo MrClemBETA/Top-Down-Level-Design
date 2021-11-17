8fd6bcc9b677dbdd561f0601e200c08c47b73111
Anthony Clemetson <aclemetson@cousd.net>
2021-11-17 11:32:44 -0800

    This is the initial commit.

---
+++ Assets/Scripts/EnterTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterTrigger : MonoBehaviour
+{
+    public string triggerName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(triggerName);
+        if(PlayerData.instance.EnterTriggerLocation == triggerName)
+        {
+            Debug.Log("This has been reached.");
+            PlayerData.instance.gameObject.transform.position = transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
